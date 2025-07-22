5528b2439f93d5c4ad1656ea5d023fabf28b0649
majdba123 <137396563+majdba123@users.noreply.github.com>
2025-07-22 12:13:49 +0300

    Update QtapAdminsController.php

--- app/Http/Controllers/QtapAdminsController.php
+++ app/Http/Controllers/QtapAdminsController.php
@@ -81,20 +81,19 @@ class QtapAdminsController extends Controller
 
         // إنشاء مصفوفة تحتوي على جميع الأشهر
         $allMonths = collect([
-            1 => 'يناير',
-            2 => 'فبراير',
-            3 => 'مارس',
-            4 => 'أبريل',
-            5 => 'مايو',
-            6 => 'يونيو',
-            7 => 'يوليو',
-            8 => 'أغسطس',
-            9 => 'سبتمبر',
-            10 => 'أكتوبر',
-            11 => 'نوفمبر',
-            12 => 'ديسمبر'
+            1 => 'Jan',
+            2 => 'Feb',
+            3 => 'Mar',
+            4 => 'Apr',
+            5 => 'May',
+            6 => 'Jun',
+            7 => 'Jul',
+            8 => 'Aug',
+            9 => 'Sep',
+            10 => 'Oct',
+            11 => 'Nov',
+            12 => 'Dec'
         ]);
-
         // دمج الأشهر مع عدد الأفرع والأرباح
         $branchesPerMonthWithAllMonths = $allMonths->map(function ($monthName, $monthNumber) use ($branchesPerMonth) {
             $branchData = $branchesPerMonth->get($monthNumber);
@@ -110,7 +109,7 @@ class QtapAdminsController extends Controller
 
 
 
-   /* public function Performance($startYear, $endYear)
+    public function Performance($startYear, $endYear)
     {
         // تحديد بداية ونهاية السنة الأولى (startYear)
         $startDate1 = $startYear . '-01-01';
@@ -147,7 +146,7 @@ class QtapAdminsController extends Controller
             'Subscriptions_' . $endYear => $Subscriptions2,
             'Orders_' . $endYear => $Orders2,
         ]);
-    }*/
+    }
 
 
 
